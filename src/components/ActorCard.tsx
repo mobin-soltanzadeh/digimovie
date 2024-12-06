707a5ca69500b29be2b4b3d1bd0d8508a6ede38b
mobin <mobin.soltanzadeh.work@gmail.com>
2024-12-06 13:12:17 +0330

    make Actors-Page responsive

--- src/components/ActorCard.tsx
+++ src/components/ActorCard.tsx
@@ -1,7 +1,16 @@
-export default function ActorCard() {
+import { TypeActor } from "../services/Datas";
+
+interface Props{
+  actor: TypeActor;
+  dataAosDuration?: string;
+}
+
+export default function ActorCard({ actor, dataAosDuration }: Props) {
   return (
-    <div className="ActorCard">
-      <div></div>
+    <div data-aos="fade-up" data-aos-duration={dataAosDuration} className="ActorCard flex flex-col justify-start items-center w-30 sm400:w-32 sm500:w-40 px-1 sm500:px-2 bg-white dark:bg-neutral-800 rounded-md">
+      <img className="aspect-[4/5] object-cover rounded-md -translate-y-2" src={actor.poster} alt="image" />
+
+      <p className="text-lg font-bold text-center line-clamp-1 -translate-y-2">{actor.name}</p>
     </div>
   );
 }
--- src/pages/Actors.tsx
+++ src/pages/Actors.tsx
@@ -1,44 +1,41 @@
-import { useParams } from "react-router-dom";
+import SideBar from "../components/SideBar";
 
 import { TypeActor, maleActors, femaleActors, directors } from "../services/Datas";
+import ActorCard from "../components/ActorCard";
 
 interface Props {
-  type: string;
+  type: "all" | "male" | "female" | "directors";
 }
 
 export default function Actors({ type }: Props) {
   let results: TypeActor[] = [];
-  let x = useParams();
-  console.log(x);
 
-  if (type === "all") results = maleActors.concat(femaleActors); // safg
+  if (type === "all") results = maleActors.concat(femaleActors).sort(); 
   else if (type === "male") results = maleActors.sort();
-  else if (type === "female") results = femaleActors;
-  else if (type === "directors") results = directors;
+  else if (type === "female") results = femaleActors.sort();
+  else if (type === "directors") results = directors.sort();
   else results = [];
 
   results = results.sort((a, b) => a.name.charCodeAt(0) - b.name.charCodeAt(0) )
 
-  // let xy = maleActors.concat(femaleActors)
-  // console.log(maleActors.length, femaleActors.length);
-  
-  // xy.forEach(val => console.log(val))
-  
-
   return (
-    <div className="grid grid-cols-5 gap-8 px-32">
-      {
-        results.map(actor => (
+    <div className="Players flex justify-between items-start gap-x-5 w-full lg:w-250 xl:w-300 mx-auto px-3 lg:px-0 mt-5">
+      <SideBar />
 
-          <div>
-            <div className="flex flex-col justify-start items-center w-44 bg-zinc-700 rounded-md">
-              <img className="w-36 aspect-[4/5] object-cover rounded-md -translate-y-3" src={actor.poster} alt="image" />
+      <div className="flex flex-col gap-y-4 ">
+        <div className="relative flex justify-center items-center font-semibold w-full h-auto bg-white dark:bg-neutral-800 rounded-lg overflow-hidden">
+            <span className="absolute top-0 left-0 flex justify-center items-center gap-x-1 text-sm sm:text-base lg:text-xl text-nowrap pl-4 pr-20 py-2 lg:py-4 bg-gradient-to-r from-zinc-400 dark:from-zinc-950 to-transparent self-start rounded-lg"><span className="text-base sm:textlg xl:text-2xl">{results.length}</span> member</span>
+            <h1 className="text-sm sm:text-base lg:text-xl py-2 lg:py-4">best {}</h1>
+        </div>
 
-              <p className="text-white text-lg font-bold -translate-y-2">{actor.name}</p>
-            </div>
-          </div>
-        ))
-      }
+        <div className="grid grid-cols-1 sm250:grid-cols-2 sm500:grid-cols-3 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 w-full mx-auto md:mx-0">
+          { results.map((actor, index) => (
+              <ActorCard dataAosDuration={index%2 === 0 ? "1500" : "3000"} key={actor.name} actor={actor} />
+          ))}
+        </div>
+      </div>
+
     </div>
+    
   );
 }
