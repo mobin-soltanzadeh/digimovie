8ade5993ae793ec08f3e66b7342b57727cf56c96
mobin <mobin.soltanzadeh.work@gmail.com>
2024-12-10 18:10:20 +0330

    fix responsive issues

--- src/components/FavoritMovieCard.tsx
+++ src/components/FavoritMovieCard.tsx
@@ -17,9 +17,9 @@ export default function FavoritMovieCard({ movieName, status, setStatus }: Props
   }
 
   return (
-  <div className="FavoritMovieCard flex justify-between items-center gap-x-1 text-lg w-full px-10 py-4 bg-gray-200 dark:bg-zinc-900 rounded-xl">
-      <Link to={"/digimovie/movie/"+movieName} className="flex justify-center items-center gap-x-2 text-lg font-serif hover:text-orange-400 line-clamp-1"><BiCameraMovie size={"25px"} color="#fb923c" /> {movieName}</Link>
-      <div className="w-6 h-6 hover:text-orange-400 cursor-pointer" onClick={clickHandler} ><IoTrashOutline size={"100%"} /> </div>
+    <div className="FavoritMovieCard flex justify-between items-center gap-x-4 text-sm lg:text-lg px-2 md:px-4 lg:px-10 py-4 bg-gray-200 dark:bg-zinc-900 rounded-xl">
+      <Link to={"/digimovie/movie/"+movieName} className="flex justify-center items-center gap-x-2 text-sm lg:text-lg font-serif hover:text-orange-400 line-clamp-1"><BiCameraMovie size={"25px"} color="#fb923c" /> {movieName}</Link>
+      <div className="w-5 h-5 lg:w-6 lg:h-6 hover:text-orange-400 cursor-pointer" onClick={clickHandler} ><IoTrashOutline size={"100%"} /> </div>
     </div>
   );
 }
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -14,16 +14,16 @@ export default function Favorites() {
   }, [reset])
   
   if(counts===0)  return (
-    <div className="mt-20 mb-40 lg:mb-72 xl:mb-96 text-center px-20 py-5 bg-gray-300 dark:bg-zinc-800">
+    <div className="mt-20 mb-40 lg:mb-72 xl:mb-96 text-center px-20 py-5 bg-gray-300 dark:bg-zinc-800 overflow-hidden">
       <h1>Ops !</h1>
       <h1 className="">You have not added any movie to your favorites list</h1>
     </div>
   );
 
   return (
-    <div className="Favorites min-h-[55vh] w-full">
+    <div className="Favorites min-h-[55vh] w-full  overflow-hidden">
       <div className="w-full bg-gray-300 dark:bg-neutral-800 p-10">
-        <ul className="flex flex-col justify-center items-center gap-y-8 w-96 mx-auto">
+        <ul className="flex flex-col justify-center items-center gap-y-8">
           { allLikedMovies.map((movie: string) => <FavoritMovieCard key={movie} movieName={movieNameConverter(movie)} status={reset} setStatus={setReset} /> ) }
         </ul>
       </div>
